ffcce5841f87606c2b694a6555ce0119d63d71f7
Julian Grisales <jgrisales@gmail.com>
2023-06-11 16:31:01 +0200

    Nav and Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image"
-
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="bg-gray-100 min-h-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-1 px-4 mr-3 rounded">
+                Signin
+              </button>
+              <button className="border p-1 px-4 rounded">Signup</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        <main>
+          {/* HEADER */}
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-5xl font-bold mb-2">
+                Find your table for any occasion
+              </h1>
+            </div>
+          </div>
+          {/* HEADER */}
+        </main>
+      </main>
     </main>
   )
 }
