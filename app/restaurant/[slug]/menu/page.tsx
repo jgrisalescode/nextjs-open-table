3fc36aa31a41cb8bde21a2fdab1dc62820854dcb
Julian Grisales <jgrisales@gmail.com>
2023-06-16 14:57:56 +0200

    Layouts Nested Layouts

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,19 +1,13 @@
 import React from "react"
-import Header from "../components/Header"
 import RestaurantNavBar from "../components/RestaurantNavBar"
 import Menu from "../components/Menu"
 
 const RestaurantMenu = (): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Menu />
-        </div>
-      </div>
-    </>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
   )
 }
 
