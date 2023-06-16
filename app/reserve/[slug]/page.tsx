c73602814e6f34e31c71b5e8efc146dc8fd765c7
Julian Grisales <jgrisales@gmail.com>
2023-06-16 14:48:44 +0200

    Layouts Root Layout

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,21 +1,15 @@
 import React from "react"
-import NavBar from "@/app/components/NavBar"
 import Header from "./components/Header"
 import Form from "./Form"
 
 const Reservation = (): JSX.Element => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   )
 }
 
